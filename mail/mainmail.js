a3be604926f75e5ff6ca247a963e58bf9700d54d
Trusimo <simonetrunfio96@gmail.com>
2022-05-23 16:46:13 +0200

    add let

--- mail/mainmail.js
+++ mail/mainmail.js
@@ -8,10 +8,14 @@ var usermail = prompt("Inserisci la tua mail")
 
 const listamail = ["rufy@gmail.com", "zoro@gmail.com","nami@gmail.com", "usopp@gmail.com", "sanji@gmail.com", "robin@gmail.com", "franky@gmail.com", "brook@gmail.com", "jinbe@gmail.com"]
 
+let existingmail;
+
 for (let i = 0; i < listamail.length; i++) {
-    if (usermail === "rufy@gmail.com" || "zoro@gmail.com" || "nami@gmail.com" || "usopp@gmail.com" || "sanji@gmail.com" || "robin@gmail.com" || "franky@gmail.com" || "brook@gmail.com" || "jinbe@gmail.com")
+
+    existingmail = listamail[i]
+
+    if (usermail === existingmail)
     {    
-        
         alert("Andiamo a cercare il One Piece!")
         console.log("Andiamo a cercare il One Piece!")
     }
