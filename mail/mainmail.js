0e1506bc1e06a344c1c27c09faebb2d6a56a63e1
Trusimo <simonetrunfio96@gmail.com>
2022-05-23 16:28:12 +0200

    trying mail

---
+++ mail/mainmail.js
@@ -0,0 +1,22 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// (non usare includes() o indexOf)
+
+var usermail = prompt("Inserisci la tua mail")
+
+const listamail = ["rufy@gmail.com", "zoro@gmail.com","nami@gmail.com", "usopp@gmail.com", "sanji@gmail.com", "robin@gmail.com", "franky@gmail.com", "brook@gmail.com", "jinbe@gmail.com"]
+
+for (let i = 0; i < listamail.length; i++) {
+    if (usermail === "rufy@gmail.com" || "zoro@gmail.com" || "nami@gmail.com" || "usopp@gmail.com" || "sanji@gmail.com" || "robin@gmail.com" || "franky@gmail.com" || "brook@gmail.com" || "jinbe@gmail.com")
+    {    
+        
+        alert("Andiamo a cercare il One Piece!")
+        console.log("Andiamo a cercare il One Piece!")
+    }
+    else {
+        alert("Non fai parte della ciurma di Cappello di paglia!")
+        console.log("Non fai parte della ciurma di Cappello di paglia!")
+    }
+}
